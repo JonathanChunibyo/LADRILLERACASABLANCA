da23d0d7fab74028f1f0263d7708472f6833cb1a
JonathanRubio <jonathanadonayrj@ufps.edu.co>
2020-12-01 10:40:59 -0500

    Get y Set usuaio - datoConsumo

---
+++ LADRILLERA_CASABLANCA/JAVA/dato_consumo.java
@@ -0,0 +1,47 @@
+
+import static com.sun.org.apache.xalan.internal.lib.ExsltDatetime.date;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author PC-ARROCERAGELVEZ
+ */
+public class dato_consumo {
+    
+    
+    String hora;
+    String minuto;
+    String fecha;
+
+    public String getHora() {
+        return hora;
+    }
+
+    public void setHora(String hora) {
+        this.hora = hora;
+    }
+
+    public String getMinuto() {
+        return minuto;
+    }
+
+    public void setMinuto(String minuto) {
+        this.minuto = minuto;
+    }
+
+    public String getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(String fecha) {
+        this.fecha = fecha;
+    }
+    
+    
+    
+}
